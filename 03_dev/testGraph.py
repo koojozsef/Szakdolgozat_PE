87a46b9b48f969986a0830373b754a3b009f0eda
Unknown <koojozsef@gmail.com>
2019-05-07 20:20:47 +0200

    test result generated

--- 03_dev/testGraph.py
+++ 03_dev/testGraph.py
@@ -3,11 +3,10 @@ import matplotlib.pyplot as plt
 
 
 def main():
-    bg_name = ["city", "nature", "sky", "tree", "waterfall"]
-    fg_name = ["butterfly", "tigger1", "tigger2", "tigger3", "walk1",
-               "walk2", "walk3", "walk4", "raptor", "girl", "dance"]
+    bg_name = ["nature", "tree", "waterfall"]
+    fg_name = ["butterfly", "walk1", "walk3", "walk4"]
 
-    dir_path = "D:/joci/projects/Szakdoga_PE/Szakdoga/Dataset/Generated/test_results/"
+    dir_path = "D:/joci/projects/Szakdoga_PE/Szakdoga/Dataset/Generated/test_results/00_"
 
     data = []
     for bg in bg_name:
@@ -17,21 +16,27 @@ def main():
             f.readline()
             f.readline()
             seq = []
-            for line_n in range(100):
+            for line_n in range(40):
                 line_str_lst = f.readline().split(",")
-                line_array = np.array(line_str_lst, dtype=float)
-                block = np.reshape(line_array[1:], (3, 4))
+                try:
+                    line_array = np.array(line_str_lst, dtype=float)
+                except:
+                    print(line_str_lst)
+                block = np.reshape(line_array[1:], (4, 4))
                 seq.append(block)
             data.append(seq)
     data_array = np.array(data)
     mog_array = data_array[:, :, 0, :]
     mog2_array = data_array[:, :, 1, :]
     knn_array = data_array[:, :, 2, :]
+    mymog_array = data_array[:, :, 3, :]
 
     mog_avg = np.average(mog_array, axis=0)
     mog2_avg = np.average(mog2_array, axis=0)
     knn_avg = np.average(knn_array, axis=0)
+    mymog_avg = np.average(mymog_array, axis=0)
 
+    """
     plt.subplot(1, 4, 1)
     plt.title("False positive (%)")
     plt.ylabel("%")
@@ -39,7 +44,8 @@ def main():
     plt.plot(mog_avg[:, 0])
     plt.plot(mog2_avg[:, 0])
     plt.plot(knn_avg[:, 0])
-    plt.legend(('MoG', 'MoG2', 'KNN'),
+    plt.plot(mymog_avg[:, 0])
+    plt.legend(('MoG', 'MoG2', 'KNN', 'My MoG'),
                loc='upper right')
 
     plt.subplot(1, 4, 2)
@@ -49,25 +55,29 @@ def main():
     plt.plot(mog_avg[:, 1])
     plt.plot(mog2_avg[:, 1])
     plt.plot(knn_avg[:, 1])
-    plt.legend(('MoG', 'MoG2', 'KNN'),
+    plt.plot(mymog_avg[:, 1])
+    plt.legend(('MoG', 'MoG2', 'KNN', 'My MoG'),
                loc='upper right')
+    """
 
-    plt.subplot(1, 4, 3)
+    plt.subplot(1, 2, 1)
     plt.title("Kappa coefficient")
     plt.xlabel("t")
     plt.plot(mog_avg[:, 2])
     plt.plot(mog2_avg[:, 2])
     plt.plot(knn_avg[:, 2])
-    plt.legend(('MoG', 'MoG2', 'KNN'),
+    plt.plot(mymog_avg[:, 2])
+    plt.legend(('MoG', 'MoG2', 'KNN', 'My MoG'),
                loc='upper right')
 
-    plt.subplot(1, 4, 4)
+    plt.subplot(1, 2, 2)
     plt.title("runtime")
     plt.xlabel("t")
     plt.plot(mog_avg[:, 3])
     plt.plot(mog2_avg[:, 3])
     plt.plot(knn_avg[:, 3])
-    plt.legend(('MoG', 'MoG2', 'KNN'),
+    plt.plot(mymog_avg[:, 3])
+    plt.legend(('MoG', 'MoG2', 'KNN', 'My MoG'),
                loc='upper right')
 
     plt.show()
